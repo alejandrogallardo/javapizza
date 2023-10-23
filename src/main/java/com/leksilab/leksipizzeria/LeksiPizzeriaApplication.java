e9ff7ddac48e3523fa870611ef7b96e4dbbefb12
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-10-23 15:39:20 -0600

    Creacion de entidades

---
+++ src/main/java/com/leksilab/leksipizzeria/LeksiPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.leksilab.leksipizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeksiPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeksiPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leksilab/leksipizzeria/LeksiPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leksilab.leksipizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeksiPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
