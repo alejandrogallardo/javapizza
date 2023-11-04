b92d8e5d4b932032fd4f6e51700c03f63ce9fcf3
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-11-04 14:37:36 -0600

    Query Methods: After, Before, Between, Count, In

--- src/main/java/com/leksilab/leksipizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/com/leksilab/leksipizzeria/persistence/repository/OrderRepository.java
@@ -3,4 +3,10 @@ package com.leksilab.leksipizzeria.persistence.repository;
 import com.leksilab.leksipizzeria.persistence.entity.OrderEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> { }
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+    List<OrderEntity> findAllByDateAfter(LocalDateTime date);
+    List<OrderEntity> findAllByMethodIn(List<String> method);
+}
