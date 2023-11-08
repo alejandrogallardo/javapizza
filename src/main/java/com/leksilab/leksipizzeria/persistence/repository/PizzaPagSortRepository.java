6b7208d44c05d145cdaf8a273ab72ef6fb4b275c
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-11-08 11:33:24 -0600

    Ordenando la información paginada

--- src/main/java/com/leksilab/leksipizzeria/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/com/leksilab/leksipizzeria/persistence/repository/PizzaPagSortRepository.java
@@ -1,7 +1,10 @@
 package com.leksilab.leksipizzeria.persistence.repository;
 
 import com.leksilab.leksipizzeria.persistence.entity.PizzaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+    Page<PizzaEntity> findByAvailableTrue(Pageable pageable);
 }
