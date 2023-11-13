92287c2321142f8b80c838936384c0f1266519b7
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-11-13 12:24:40 -0600

    Usar la anotación @Query con JPQL

---
+++ src/main/java/com/leksilab/leksipizzeria/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.leksilab.leksipizzeria.service;
+
+import com.leksilab.leksipizzeria.persistence.entity.CustomerEntity;
+import com.leksilab.leksipizzeria.persistence.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    public CustomerEntity findByPhone(String phone) {
+        return this.customerRepository.findByPhone(phone);
+    }
+}
