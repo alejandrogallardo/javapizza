94d7ae5419b4bdf9bad6feacf6bf376c1fb5d8a4
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2023-12-06 13:07:50 -0600

    Auditoría con Spring Data

---
+++ src/main/java/com/leksilab/leksipizzeria/persistence/entity/AuditableEntity.java
@@ -0,0 +1,21 @@
+package com.leksilab.leksipizzeria.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AuditableEntity {
+    @Column(name = "created_date")
+    @CreatedDate
+    @JsonIgnore // Con esto no se muestra en la respuesta este campo
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
--- src/main/java/com/leksilab/leksipizzeria/persistence/entity/PizzaEntity.java
+++ src/main/java/com/leksilab/leksipizzeria/persistence/entity/PizzaEntity.java
@@ -1,16 +1,23 @@
 package com.leksilab.leksipizzeria.persistence.entity;
 
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+//import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
@@ -33,4 +40,17 @@ public class PizzaEntity {
 
     @Column(columnDefinition = "TINYINT", nullable = false)
     private Boolean available;
+
+    /*
+    * Para no tener que crear estas dos columnas en cada entidad se procede a crear una nueva entidad
+    * llamada AuditableEntity
+    * */
+    /*@Column(name = "created_date")
+    @CreatedDate
+    @JsonIgnore // Con esto no se muestra en la respuesta este campo
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;*/
 }
